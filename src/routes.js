7035bce58648428242608751e02fa849d260856c
Vinicius <vinidev96@gmail.com>
2024-05-02 14:35:06 -0300

    refactor: add auth provider logic to handle private routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,34 @@
-import React from 'react';
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AuthContext from './contexts/AuthContext';
 import MainPage from './pages/MainPage';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './components/NotFound';
 
 export default function RoutesMap() {
+    const { isLoggedIn } = useContext(AuthContext);
+
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<MainPage />} />
-                <Route exact path='/cart' element={<Cart />} />
-                <Route exact path="/product/:id" element={<Product />} />
+                {isLoggedIn ? (
+                    <>
+                        <Route exact path="/" element={<MainPage />} />
+                        <Route exact path="/cart" element={<Cart />} />
+                        <Route exact path="/product/:slug" element={<Product />} />
+                        <Route path="*" element={<Navigate to="/404" />} />
+                    </>
+                ) : (
+                    <>
+                        <Route exact path="/login" element={<Login />} />
+                        <Route exact path="/register" element={<Register />} />
+                        <Route path="*" element={<Navigate to="/login" />} />
+                    </>
+                )}
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
