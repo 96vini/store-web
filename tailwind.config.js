291f4456c89755130463f8712ad05ba2d487899c
Vinicius <vinidev96@gmail.com>
2024-05-02 19:18:47 -0300

    styles: add some variables from colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
       colors: {
         primary: '#222',
         secondary: '#666',
-        accent: '#111',
+        accent: '#6667AB',
+        active: '#43448e',
+        accent2: '#2fbfb3',
+        active2: '#229b91',
+        accentdark: '#43447c',
+        boreau: {
+          start: '#575892',
+          end: '#32b4af',
+        }
       },
     },
   },
