4b32ac4bb0901b18178e4ec107954afef8c08175
Vinicius <vinidev96@gmail.com>
2024-05-02 19:15:40 -0300

    feat: add new style to product view

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,18 +1,39 @@
 import React, { useState, useEffect } from "react";
+
 import Navbar from "../../components/Navbar";
+
 import ProductView from "../../components/ProductView";
+import ProductNotFound from '../../components/ProductNotFound';
+
+import { useParams } from "react-router";
+import api from "../../services/api";
 
 export default function Product() {
-    const [product, setProducts] = useState({
-        price: 50.00,
-        name: 'Camiseta do Corinthians'
-    });
+
+    const { slug } = useParams();
+    const [product, setProduct] = useState(null);
+
+    useEffect(() => {
+        getProductBySlug(slug);
+    }, []);
+
+    async function getProductBySlug(slug) {
+        await api.get(`/product/${slug}`).then(res => {
+            setProduct(res.data);
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
     return (
         <div className="w-full h-screen">
             <Navbar />
             <div className="w-full h-auto flex justify-center">
-                <ProductView product={product}/>
+                {product ? (
+                    <ProductView product={product}/>
+                ) : (
+                    <ProductNotFound />
+                )}
             </div>
         </div>
     );
